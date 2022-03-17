27692f21aeb8bf04d934d8fb4730741580c0d5cb
Albert Farrakhov <albert.farrakhov@tii.ae>
2022-03-17 12:26:52 -0700

    Add IR mode with angr support

--- backend/angr/mdec-angr/mdecangr/service.py
+++ backend/angr/mdec-angr/mdecangr/service.py
@@ -1,7 +1,56 @@
 import traceback
+from typing import Tuple, Type
 
-from mdecbase import Service
 import angr
+import pyvex
+from mdecbase import Service
+
+
+class FunctionVisitor:
+    def __init__(self, project: angr.Project):
+        self._project = project
+
+    @classmethod
+    def name(cls) -> str:
+        raise NotImplementedError()
+
+    @property
+    def project(self) -> angr.Project:
+        return self._project
+
+    def visit(self, func: angr.knowledge_plugins.functions.Function) -> str:
+        raise NotImplementedError()
+
+
+class DecompilerFunctionVisitor(FunctionVisitor):
+    def __init__(self, project: angr.Project):
+        super(DecompilerFunctionVisitor, self).__init__(project)
+
+    def name(cls) -> str:
+        return 'Decompiler'
+
+    def visit(self, func: angr.knowledge_plugins.functions.Function) -> str:
+        dec = self.project.analyses.Decompiler(func)
+        return dec.codegen.text if dec.codegen else ''
+
+
+class IRFunctionVisitor(FunctionVisitor):
+    def __init__(self, project: angr.Project):
+        super(IRFunctionVisitor, self).__init__(project)
+
+    def name(cls) -> str:
+        return 'PyVEX'
+
+    def visit(self, func: angr.knowledge_plugins.functions.Function) -> str:
+        func_vex = [f'--- {func.name} ---']
+        for block in func.blocks:
+            try:
+                func_vex.append(
+                    str(self.project.factory.block(block.addr).vex))
+            except (angr.AngrError, pyvex.PyVEXError):
+                func_vex.append(
+                    f'/* Producing IR of {block} failed:\n{traceback.format_exc()}\n*/')
+        return '\n'.join(func_vex)
 
 
 class AngrService(Service):
@@ -9,10 +58,8 @@ class AngrService(Service):
     angr decompiler as a service
     """
 
-    def decompile(self, path: str) -> str:
-        """
-        Decompile all the functions in the binary located at `path`.
-        """
+    @staticmethod
+    def _process_binary(path: str) -> Tuple[angr.Project, angr.analyses.CFG]:
         p = angr.Project(path, auto_load_libs=False, load_debug_info=True)
         cfg = p.analyses.CFG(normalize=True,
                              resolve_indirect_jumps=True,
@@ -23,21 +70,42 @@ class AngrService(Service):
             cfg=cfg,
             recover_variables=True,
             analyze_callsites=True
-            )
+        )
+        return p, cfg
+
+    @staticmethod
+    def _process_functions(path: str, visitor_cls: Type[FunctionVisitor]) -> str:
+        project, cfg = AngrService._process_binary(path)
+        visitor = visitor_cls(project)
         funcs = [func for func in cfg.functions.values()
-                  if not func.is_plt
-                  and not func.is_simprocedure
-                  and not func.alignment
-                  ]
+                 if not func.is_plt
+                 and not func.is_simprocedure
+                 and not func.alignment
+                 ]
         out = []
         for func in funcs:
             try:
-                dec = p.analyses.Decompiler(func)
-                out.append(dec.codegen.text)
-            except:
-                out.append('/* Decompilation of %s failed:\n%s\n*/' % (func, traceback.format_exc()))
+                out.append(visitor.visit(func))
+            except angr.AngrError:
+                out.append(
+                    f'/* {visitor_cls.name()} processing {func} failed:\n{traceback.format_exc()}\n*/')
 
         return '\n'.join(out)
 
+    def decompile(self, path: str) -> str:
+        """
+        Decompile all the functions in the binary located at `path`.
+        """
+        return AngrService._process_functions(path, DecompilerFunctionVisitor)
+
+    def ir(self, path: str) -> str:
+        """
+        Generate VEX IR for all functions in the binary located at `path`.
+        """
+        return AngrService._process_functions(path, IRFunctionVisitor)
+
     def version(self) -> str:
         return '.'.join(str(i) for i in angr.__version__)
+
+    def ir_version(self) -> str:
+        return '.'.join(str(i) for i in pyvex.__version__)
--- backend/common/mdec-base/mdecbase/service.py
+++ backend/common/mdec-base/mdecbase/service.py
@@ -1,6 +1,7 @@
+import argparse
 import tempfile
 import traceback
-import argparse
+from typing import Callable, Type
 
 from aiohttp import web
 import aiohttp
@@ -13,16 +14,26 @@ class Service:
 
     def __init__(self):
         self.app = web.Application()
-        self.app.add_routes([web.post('/decompile', self.post_decompile)])
-        self.app.add_routes([web.get('/version', self.get_version)])
+        self.app.add_routes([web.post('/decompile', self.post_decompile),
+                             web.get('/decompile/version',
+                                     self.get_decompiler_version),
+                             web.post('/ir', self.post_ir),
+                             web.get('/ir/version', self.get_ir_version)])
 
     def decompile(self, path: str) -> str:
+        raise NotImplementedError()
+
+    def ir(self, path: str) -> str:
         raise NotImplementedError()
 
     def version(self) -> str:
         raise NotImplementedError()
 
-    async def post_decompile(self, request: aiohttp.web.BaseRequest) -> web.Response:
+    def ir_version(self) -> str:
+        return self.version()
+
+    @staticmethod
+    async def __process_file(request: aiohttp.web.BaseRequest, callback: Callable[[str], str]) -> web.Response:
         reader = await request.multipart()
         binary = await reader.next()
         if binary is None:
@@ -37,30 +48,43 @@ class Service:
                 f.flush()
 
             try:
-                decomp = self.decompile(f.name)
+                output = callback(f.name)
                 resp_status = 200
             except:
-                decomp = traceback.format_exc()
+                output = traceback.format_exc()
                 resp_status = 500
 
-            return web.Response(text=decomp, status=resp_status)
+            return web.Response(text=output, status=resp_status)
 
-    async def get_version(self, request: aiohttp.web.BaseRequest) -> web.Response:
+    async def post_decompile(self, request: aiohttp.web.BaseRequest) -> web.Response:
+        return await Service.__process_file(request, self.decompile)
+
+    async def post_ir(self, request: aiohttp.web.BaseRequest) -> web.Response:
+        return await Service.__process_file(request, self.ir)
+
+    async def _get_version(self, callback: Callable[[], str]) -> web.Response:
         try:
-            version = self.version()
+            version = callback()
             resp_status = 200
         except:
             version = traceback.format_exc()
             resp_status = 500
         return web.Response(text=version, status=resp_status)
 
+    async def get_decompiler_version(self, _request: aiohttp.web.BaseRequest) -> web.Response:
+        return await self._get_version(self.version)
 
-def mdec_main(service: Service):
+    async def get_ir_version(self, _request: aiohttp.web.BaseRequest) -> web.Response:
+        return await self._get_version(self.ir_version)
+
+
+def mdec_main(service: Type[Service]):
     """
     Common module main function
     """
     ap = argparse.ArgumentParser()
-    ap.add_argument('file', nargs='?', help='If provided, decompile given file and exit. Otherwise, start server')
+    ap.add_argument('file', nargs='?',
+                    help='If provided, decompile given file and exit. Otherwise, start server')
     args = ap.parse_args()
 
     s = service()
